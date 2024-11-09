24f326f197166f2ff37b379f8621642191b6e679
surajllpl <srj.llpl@gmail.com>
2024-11-09 18:22:01 +0530

    Website - Animated Landing page

---
+++ web/main.js
@@ -0,0 +1,29 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click",(e)=>{
+    navLinks.classList.toggle("open");
+    const isOpen =navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class",isOpen? "ri-close-line":"ri-menu-line")
+});
+
+navLinks.addEventListener("click",(e)=>{
+    navLinks.classList.remove('open');
+    menuBtnIcon.setAttribute("class","ri-menu-line");
+});
+
+const scrollRevealOption ={
+    distance :"50px",
+    origin : "bottom",
+    duration : 1000,
+};
+
+ScrollReveal().reveal(".container_left h1",{
+    ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".container_left .container_btn",{
+    ...scrollRevealOption,
+    delay: 500,
+});
